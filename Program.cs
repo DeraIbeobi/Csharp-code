3861bda7d7ef6eb2f36be772a1347e075187c720
DeraIbeobi <40524298+DeraIbeobi@users.noreply.github.com>
2023-03-03 06:19:56 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int i = 1000;
+            byte byt = (byte) i;
+            System.Console.WriteLine(byt);
+
+            var number = "1234";
+            int ii = Convert.ToInt32(number);
+            System.Console.WriteLine(ii);
+
+            
+            var aa = 2;
+            var bb = 4;
+            var ac = 6;
+            System.Console.WriteLine((float)(aa * bb) / (float)ac);
+
+            var a = 1;
+            var b = 2;
+            var c = 3;
+            System.Console.WriteLine(c > b && c > a);
+            System.Console.WriteLine(!(c > b || c == a));
+
+
+            /*try
+            {
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+                System.Console.WriteLine(b);
+            }
+            catch (System.Exception)
+            {
+                System.Console.WriteLine("The number could not be converted to a byte");
+                throw;
+            }*/
+        }
+    }
+}
